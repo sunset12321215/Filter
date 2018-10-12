9d47fd5f788ac48bd079b80d918037fb864dcafd
sunset12321215 <sunsetno08@gmail.com>
2018-10-12 21:06:06 +0700

    Tạo Filter

--- Filter/ViewController.swift
+++ Filter/ViewController.swift
@@ -10,16 +10,46 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    //  Ánh xạ
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    
+    //  Action
+    
+    @IBAction func bt_Add(_ sender: Any) {
     }
-
-
+    
+    
+    @IBAction func btn_Filter(_ sender: Any)
+    {
+        //  1.  Tạo bảng lựa chọn
+        let sheet = UIAlertController(title: "Lựa chọn lọc", message: nil, preferredStyle: .actionSheet)
+        
+        //  2.  Thêm lựa chọn
+        sheet.addAction(UIAlertAction(title: "Lựa chọn 1", style: .default, handler: { (action) in
+            //
+        }))
+        
+        
+        sheet.addAction(UIAlertAction(title: "Lựa chọn 2", style: .default, handler: { (action) in
+            //
+        }))
+        
+        
+        //  3.  Huỷ
+        sheet.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: { (action) in
+            //
+        }))
+        
+        //  4.  Hiện bảng lựa chọn
+        self.present(sheet, animated: true, completion: nil)
+    }
 }
 
